1105249c4a9aff282251ce9638031bc7f8ed9471
unknown <online7890@gmail.com>
2021-08-06 06:56:21 +0300

    initial commit

---
+++ src/LiveClick.ts
@@ -0,0 +1,35 @@
+interface ILiveClickStore {
+  items: ILiveClickItem[];
+}
+
+interface ILiveClickItem {
+  selector: string;
+  listener: ILiveClickListener;
+}
+
+type ILiveClickListener = (event: MouseEvent, target: Element) => void;
+
+const store: ILiveClickStore = {
+  items: [],
+};
+
+const on = (selector: string, listener: ILiveClickListener) => {
+  store.items.push({ selector, listener });
+};
+
+const off = (selector: string, listener: ILiveClickListener) => {
+  store.items = store.items.filter(
+    (item) => item.selector !== selector && item.listener !== listener,
+  );
+};
+
+document.addEventListener("click", (event) => {
+  store.items.forEach((item) => {
+    const target =
+      (event.target as Element | null)?.closest(item.selector) || null;
+
+    if (target !== null) {
+      item.listener(event, target);
+    }
+  });
+});
